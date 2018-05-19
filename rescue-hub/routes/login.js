dfee7a7c4b466cf0e3233e587c89a73e2c04517c
abecel22 <abe2211@gmail.com>
2018-05-19 18:12:28 -0500

    Added login page.

--- rescue-hub/routes/login.js
+++ rescue-hub/routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('respond with a source');
+    res.render('login');
 });
 
 module.exports = router;
