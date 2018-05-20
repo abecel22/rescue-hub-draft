02c276a938ebd09f2e187e52d75b36ab51424603
abecel22 <abe2211@gmail.com>
2018-05-19 21:14:16 -0500

    Added faqs page in express.

--- rescue-hub/app.js
+++ rescue-hub/app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 var rescueRouter = require('./routes/rescue');
+var registerRouter = require('./routes/register');
+var faqRouter = require('./routes/faq');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/rescue', rescueRouter);
+app.use('/register', registerRouter);
+app.use('/faq', faqRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
